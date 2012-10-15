256a94b50f3abf4f0c5a0f04250dbf23bdcd8436
kurron <kurr@kurron.org>
2012-10-15 15:16:39 -0400

    Gots Unicode sample working.

--- src/test/groovy/org/kurron/pdf/clown/ClownTest.groovy
+++ src/test/groovy/org/kurron/pdf/clown/ClownTest.groovy
@@ -9,43 +9,143 @@ import org.pdfclown.documents.interchange.metadata.Information
 import org.pdfclown.files.File
 import org.pdfclown.files.SerializationModeEnum
 import spock.lang.Specification
-
+import org.pdfclown.documents.contents.fonts.Font
 import java.awt.geom.Point2D
+import org.pdfclown.documents.contents.composition.BlockComposer
+import java.awt.Dimension
+import java.awt.geom.Rectangle2D
+
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+
+import org.pdfclown.documents.Document;
+import org.pdfclown.documents.Page;
+import org.pdfclown.documents.contents.composition.AlignmentXEnum;
+import org.pdfclown.documents.contents.composition.AlignmentYEnum;
+import org.pdfclown.documents.contents.composition.BlockComposer;
+import org.pdfclown.documents.contents.composition.PrimitiveComposer;
+import org.pdfclown.documents.contents.fonts.Font;
+import org.pdfclown.files.File;
 
 /**
  * Learning test for the PDF Clown library.
  */
 class ClownTest extends Specification
 {
+    private static final float Margin = 36
+
     def "hello"( )
     {
-        given: "no added text"
+        given: "document"
         File file = new File()
         Document document = file.document
 
-        when: "build is called"
+        when: "page is built"
         Page page = new Page( document )
         document.getPages().add( page )
         PrimitiveComposer composer = new PrimitiveComposer( page )
-        composer.setFont( new StandardType1Font( document,
-                                                 StandardType1Font.FamilyEnum.Courier,
-                                                 true,
-                                                 false ),
-                                                 32 )
+        composer.setFont(                               new StandardType1Font( document,
+                                                                               StandardType1Font.FamilyEnum.Courier,
+                                                                               true,
+                                                                               false ),
+                                                        32 )
         composer.showText( "Hello World!",
                            new Point2D.Double( 32, 48 ) )
         composer.flush()
-        serialize( file, false, "Hello world", "a simple 'hello world'" )
+        serialize( file, "hello-world", false, "Hello world", "a simple 'hello world'" )
 
         then: "pdf file is created"
     }
 
-    protected void serialize( File file,
-                              boolean chooseMode,
-                              String title,
-                              String subject )
+    def "unicode"( )
     {
-        serialize( file, getClass().getSimpleName(), chooseMode, title, subject )
+        given: "document"
+        File file = new File()
+        Document document = file.document
+
+        when: "page is built"
+        Page page = new Page( document )
+        document.getPages().add( page )
+        PrimitiveComposer composer = new PrimitiveComposer( page )
+        BlockComposer blockComposer = new BlockComposer( composer )
+        final URI uri = getClass().getClassLoader().getResource( "GenR102.TTF" ).toURI()
+        assert uri
+        java.io.File bob = new java.io.File( uri )
+        Font font = Font.get( document, bob )
+        Dimension breakSize = new Dimension( 0, 10 )
+        String[] titles =
+            ["ΑΡΘΡΟ 1",
+                    "ASARIYA SINTE (1)",
+                    "Article 1",
+                    "Article premier",
+                    "Статья 1",
+                    "Artículo 1",
+                    "Artikel 1",
+                    "Madde 1",
+                    "Artikel 1",
+                    "Articolo 1",
+                    "Artykuł 1",
+                    "Bend 1",
+                    "Abala kìíní."]
+        String[] bodies =
+            ["'Ολοι οι άνθρωποι γεννιούνται ελεύθεροι και ίσοι στην αξιοπρέπεια και τα δικαιώματα. Είναι προικισμένοι με λογική και συνείδηση, και οφείλουν να συμπεριφέρονται μεταξύ τους με πνεύμα αδελφοσύνης.",
+                    "Aduniya kuna n gu ibuna damayo hɛi nɔ dei-dei nn daama nna n burucinitɛrɛ fɔ, n lasabu nna laakari ya nam nn mɔ huro cɛrɛ kuna nyanze tɛrɛ bɔŋɔɔ.",
+                    "All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.",
+                    "Tous les êtres humains naissent libres et égaux en dignité et en droits. Ils sont doués de raison et de conscience et doivent agir les uns envers les autres dans un esprit de fraternité.",
+                    "Все люди рождаются свободными и равными в своем достоинстве и правах. Они наделены разумом и совестью и должны поступать в отношении друг друга в духе братства.",
+                    "Todos los seres humanos nacen libres e iguales en dignidad y derechos y, dotados como están de razón y conciencia, deben comportarse fraternalmente los unos con los otros.",
+                    "Alle Menschen sind frei und gleich an Würde und Rechten geboren. Sie sind mit Vernunft und Gewissen begabt und sollen einander im Geist der Brüderlichkeit begegnen.",
+                    "Bütün insanlar hür, haysiyet ve haklar bakımından eşit doğarlar. Akıl ve vicdana sahiptirler ve birbirlerine karşı kardeşlik zihniyeti ile hareket etmelidirler.",
+                    "Alla människor är födda fria och lika i värde och rättigheter. De har utrustats med förnuft och samvete och bör handla gentemot varandra i en anda av gemenskap.",
+                    "Tutti gli esseri umani nascono liberi ed eguali in dignità e diritti. Essi sono dotati di ragione e di coscienza e devono agire gli uni verso gli altri in spirito di fratellanza.",
+                    "Wszyscy ludzie rodzą się wolni i równi pod względem swej godności i swych praw. Są oni obdarzeni rozumem i sumieniem i powinni postępować wobec innych w duchu braterstwa.",
+                    "Hemû mirov azad û di weqar û mafan de wekhev tên dinyayê. Ew xwedî hiş û şuûr in û divê li hember hev bi zihniyeteke bratiyê bilivin.",
+                    "Gbogbo ènìyàn ni a bí ní òmìnira; iyì àti è̟tó̟ kò̟ò̟kan sì dó̟gba. Wó̟n ní è̟bùn ti làákàyè àti ti è̟rí-o̟kàn, ó sì ye̟ kí wo̟n ó máa hùwà sí ara wo̟n gé̟gé̟ bí o̟mo̟ ìyá."]
+
+        String[] sources =
+            ["http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=grk",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=den",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=eng",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=frn",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=rus",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=spn",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=ger",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=trk",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=swd",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=itn",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=pql",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=kdb1",
+                    "http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=yor"]
+
+        final ted = new Rectangle2D.Double( Margin,
+                                            Margin,
+                                            page.getSize().getWidth() - Margin * 2,
+                                            page.getSize().getHeight() - Margin * 2 )
+
+        blockComposer.begin( ted, AlignmentXEnum.Justify, AlignmentYEnum.Top )
+
+        int length = titles.length
+        for( int index = 0; index < length;  index++ )
+        {
+            composer.setFont( font, 12 );
+            blockComposer.showText( titles[index] );
+            blockComposer.showBreak();
+
+            composer.setFont( font, 11 );
+            blockComposer.showText( bodies[index] );
+            blockComposer.showBreak( AlignmentXEnum.Right );
+
+            composer.setFont( font, 8 );
+            blockComposer.showText( "[Source: " + sources[index] + "]" );
+            blockComposer.showBreak( breakSize, AlignmentXEnum.Justify );
+        }
+
+        blockComposer.end()
+        composer.flush()
+
+        serialize( file, "unicode", false, "Unicode", "using Unicode fonts" )
+
+        then: "pdf file is created"
     }
 
     protected void serialize( File file,
